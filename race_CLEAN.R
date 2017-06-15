7d1e50bc7864ac390d39c5a8783e151ab25fd7a8
James Edwards <j.edwards4@lancaster.ac.uk>
2017-06-15 21:23:38 +0100

    Adds code to clean the two csvs to race_CLEAN.R

--- race_CLEAN.R
+++ race_CLEAN.R
@@ -5,7 +5,7 @@
 # to Results_CSV.
 ################
 
-library(pdftools)
+library(tidyverse)
 library(stringr)
 source("race_FUNC.R")
 
@@ -18,8 +18,7 @@ if(dir.exists("C:/Users/edwards/Dropbox/Mine/Personal/Running/Races&events/")){
 files_raw <- list.files(str_c(path_root, "Results_raw"))
 files_raw
 
-choose <- 8
-file_name <- str_c(path_root, "Results_raw/", files_raw[choose])
+file_name <- str_c(path_root, "Results_raw/HuttonRoof2015.txt")
 
 #############
 # HuttonRoof2015.txt
@@ -47,3 +46,48 @@ for (i in 3 : 246){
 #Save
 cat(txt, file=file_name, sep="\n")
 
+###############
+# ColedaleHS2016.csv
+###############
+file_name <- str_c(path_root, "Results_raw/ColedaleHS2016raw.csv")
+res <- read_csv(file_name)
+# create new column "name" 
+temp <- res %>% transmute(name=str_c(forename, " ", str_to_title(surname)))
+
+# replace missing values
+res_new <- res %>% replace_na(replace=list(club="UA")) %>% 
+  select(c(-1,-2))
+res_new <- bind_cols(temp, res_new)
+#Save
+file_name <- str_c(path_root, "Results_csv/ColedaleHS2016.csv")
+write.csv(res_new, file=file_name, row.names=F)
+
+###############
+# WartonCrag2016.csv
+###############
+
+#############
+#Inputs a string time written as mm.ss and returns string as hh:mm:ss.
+#Note the input may be in form "mm" which means mm mins and 0 seconds or
+# "mm.d" which means mm mins and d0 seconds.
+#############
+correct_time_format <- function(time){
+  if (!str_detect(time, "\\d")){return(time)}
+  time <- as.numeric(time)
+  mins <- floor(time)
+  secs <- round(100 * (time - mins), 2)
+  return(str_c(c(str_pad(mins%/%60, 2, pad = "0"), str_pad(mins%%60, 2, pad = "0"), 
+               str_pad(secs, 2, pad="0")), collapse=":"))
+}
+
+file_name <- str_c(path_root, "Results_raw/WartonCrag2016raw.csv")
+res <- read_csv(file_name)
+res
+
+res_new <- res %>% mutate(name=str_c(Forename, " ", Surname)) %>%
+  select(name, club=Club, cat=`Vet Category`, time=Time) %>%
+  rowwise %>%
+  mutate(time=correct_time_format(time)) 
+
+file_name <- str_c(path_root, "Results_csv/WartonCrag2016.csv")
+write.csv(res_new, file=file_name, row.names=F)
